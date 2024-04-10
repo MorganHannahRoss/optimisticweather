1f2e72ab0ffd1ec14d62dae0c34e028cc2460ef1
Matthew Nanthameechai <mnanthameechai@gmail.com>
2024-04-11 10:40:41 +1200

    displaying current weather

--- client/components/WeatherPanel/WeatherInfo.tsx
+++ client/components/WeatherPanel/WeatherInfo.tsx
@@ -14,10 +14,27 @@ function WeatherInfo() {
   if (isPending) {
     return <p>...Loading</p>
   }
+
+  // icon
+  // temp
+  // wind
+  // what it feels like
+  // houerly weather
   return (
     <>
       <div>
         <p>Hello, WeatherInfo World!</p>
+        <p>current weather</p>
+        <p>{data.current.cloud_cover}</p>
+        <p>{data.current.icon}</p>
+        <p>{data.current.icon_num}</p>
+        <p>{data.current.precipitation.total}</p>
+        <p>{data.current.precipitation.type}</p>
+        <p>{data.current.summary}</p>
+        <p>{data.current.temperature}</p>
+        <p>{data.current.wind.angle}</p>
+        <p>{data.current.wind.dir}</p>
+        <p>{data.current.wind.speed}</p>
       </div>
     </>
   )
