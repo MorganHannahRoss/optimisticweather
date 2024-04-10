3eed273dabde63e5426b4640057ffff02d3922b5
tempo <tempo@PJ-766262-K01>
2024-04-11 07:38:59 +1200

    basic nav setup

--- client/components/Nav/Nav.tsx
+++ client/components/Nav/Nav.tsx
@@ -1,8 +1,14 @@
+import { NavLink } from "react-router-dom"
+
 function Nav() {
   return (
     <>
       <div>
         <p>Hello, Nav World!</p>
+        <NavLink to='/activities'>Activities</NavLink>
+        <NavLink to='/events'>Events</NavLink>
+        <NavLink to='/clothing'>clothing</NavLink>
+        <NavLink to='/ai'>ai</NavLink>
       </div>
     </>
   )
