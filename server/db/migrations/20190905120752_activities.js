f23669e1382cf29f1680a6c74c2b7d4f5caf910d
Morgan Ross <morgan.ross@developersinstitute.co.nz>
2024-04-11 10:49:45 +1200

    adjusting migration file

---
+++ server/db/migrations/20190905120752_activities.js
@@ -0,0 +1,11 @@
+export async function up(knex) {
+  return knex.schema.createTable('activities', (table) => {
+    table.increments('id')
+    table.string('activity')
+    table.string('weather')
+  })
+}
+
+export async function down(knex) {
+  return knex.schema.dropTable('activities')
+}
