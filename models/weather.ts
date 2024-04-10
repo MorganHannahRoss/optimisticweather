5c45dffc61eeee85564054d75bd473923dd22092
Matthew Nanthameechai <mnanthameechai@gmail.com>
2024-04-11 09:58:33 +1200

    removed activity model

--- models/weather.ts
+++ models/weather.ts
@@ -1,74 +1,67 @@
-// interface Activity {
-//   activity_id: number
-//   name: string
-//   weather: string
-//   type: string
-//   optionaldetails: string
-// }
 export interface Weather {
-  lat:       string;
-  lon:       string;
-  elevation: number;
-  timezone:  string;
-  units:     string;
-  current:   Current;
-  hourly:    Hourly;
-  daily:     Daily;
+  lat: string
+  lon: string
+  elevation: number
+  timezone: string
+  units: string
+  current: Current
+  hourly: Hourly
+  daily: Daily
 }
 
 export interface Current {
-  icon:          string;
-  icon_num:      number;
-  summary:       string;
-  temperature:   number;
-  wind:          Wind;
-  precipitation: Precipitation;
-  cloud_cover:   number;
+  icon: string
+  icon_num: number
+  summary: string
+  temperature: number
+  wind: Wind
+  precipitation: Precipitation
+  cloud_cover: number
 }
 
 export interface Precipitation {
-  total: number;
-  type:  string;
+  total: number
+  type: string
 }
 
 export interface Wind {
-  speed: number;
-  angle: number;
-  dir:   string;
+  speed: number
+  angle: number
+  dir: string
 }
 
 export interface Daily {
-  data: DailyDatum[];
+  data: DailyDatum[]
 }
 
 export interface DailyDatum {
-  day:       Date;
-  weather:   string;
-  icon:      number;
-  summary:   string;
-  all_day:   AllDayElement;
-  morning:   null;
-  afternoon: null;
-  evening:   null;
+  day: Date
+  weather: string
+  icon: number
+  summary: string
+  all_day: AllDayElement
+  morning: null
+  afternoon: null
+  evening: null
 }
 
 export interface AllDayElement {
-  weather:          string;
-  icon:             number;
-  temperature:      number;
-  temperature_min?: number;
-  temperature_max?: number;
-  wind:             Wind;
-  cloud_cover:      CloudCover;
-  precipitation:    Precipitation;
-  date?:            Date;
-  summary?:         string;
+  weather: string
+  icon: number
+  temperature: number
+  temperature_min?: number
+  temperature_max?: number
+  wind: Wind
+  cloud_cover: CloudCover
+  precipitation: Precipitation
+  date?: Date
+  summary?: string
 }
 
 export interface CloudCover {
-  total: number;
+  total: number
 }
 
 export interface Hourly {
-  data: AllDayElement[];
+  data: AllDayElement[]
 }
