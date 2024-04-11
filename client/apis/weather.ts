ea6db10d4d386951e7d537ce87b72707007a28cb
Matthew Nanthameechai <mnanthameechai@gmail.com>
2024-04-12 10:52:28 +1200

    api key on client side api doesnt work for windows if in env

--- client/apis/weather.ts
+++ client/apis/weather.ts
@@ -1,9 +1,11 @@
 import request from 'superagent'
 import { Weather } from '../../models/weather'
 
-const rootURL = '/api/v1/weather'
+// const rootURL = '/api/v1/weather'
 
 export async function getWeather(): Promise<Weather> {
-  const response = await request.get(`${rootURL}`)
+  const response = await request.get(
+    `https://www.meteosource.com/api/v1/free/point?lat=41S&lon=174E&sections=all&timezone=auto&language=en&units=auto&key=fih69ecqhwqbv51n1vtauic663kn3v40ed36xhyo`,
+  )
   return response.body
 }
