f2fd6d72f7754e7ec50ee6d16a0f43721ad755bb
tempo <tempo@PJ-766262-K01>
2024-04-12 08:27:46 +1200

    fixed api key

--- client/apis/weather.ts
+++ client/apis/weather.ts
@@ -1,9 +1,7 @@
 import request from 'superagent'
 import { Weather } from '../../models/weather'
 
-// const rootURL = '/api/v1/weather'
-const weatherApiKey = 'fih69ecqhwqbv51n1vtauic663kn3v40ed36xhyo'
-const rootURL = `https://www.meteosource.com/api/v1/free/point?lat=41S&lon=174E&sections=all&timezone=auto&language=en&units=auto&key=${weatherApiKey}`
+const rootURL = '/api/v1/weather'
 
 export async function getWeather(): Promise<Weather> {
   const response = await request.get(`${rootURL}`)
