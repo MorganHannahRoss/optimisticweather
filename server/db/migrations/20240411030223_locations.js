2992f0d6f248d3fa123dc8ea71db4768a85c55cd
Morgan Ross <morgan.ross@developersinstitute.co.nz>
2024-04-11 15:32:23 +1200

    Added migration file for locations

---
+++ server/db/migrations/20240411030223_locations.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function up(knex) {
+  await knex.schema.createTable('location', (table) => {
+    table.increments('id')
+    table.string('city')
+    table.string('lat')
+    table.string('lon')
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function down(knex) {
+  return knex.schema.dropTable('location')
+}
