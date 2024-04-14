3c28e5a75a1fab1b900884c54ffea2d1abd7b0d5
Matthew Nanthameechai <mnanthameechai@gmail.com>
2024-04-15 10:58:38 +1200

    added promise

--- client/apis/eventfindaApi.ts
+++ client/apis/eventfindaApi.ts
@@ -3,7 +3,7 @@ import { Event } from '../../models/event'
 
 const rootURL = '/api/v2/eventfinda'
 
-export async function getEvent() {
+export async function getEvent(): Promise<Event> {
   const response = await request.get(`${rootURL}`)
   return response.body
 }
