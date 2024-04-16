8ee8c96f59b662b0057a147b37d90d348cac780d
tempo <tempo@PJ-766262-K01>
2024-04-17 10:39:35 +1200

    local storage fix & clean up w console.logs w 99% of comments

--- server/routes/chatbot.ts
+++ server/routes/chatbot.ts
@@ -7,14 +7,12 @@ export default router
 router.post('/', async (req, res) => {
   const { chats, currentLocation, currentWeather } = req.body
   try {
-    // Call the function from your chatbot file to get the chat response
     const chatResponse = await createChatCompletion(
       chats,
       currentLocation,
       currentWeather,
     )
 
-    // return chat response
     res.status(200).json(chatResponse)
   } catch (error) {
     console.error('Error:', error)
